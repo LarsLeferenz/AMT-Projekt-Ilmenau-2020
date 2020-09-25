4a01918ed886fbe1e9b574e9579f69bfc2ceea11
Larsinator9000 <lars.leferenz@gmail.com>
2020-09-25 19:20:16 +0200

    UI-Fixes + Blinker

--- Fahrsteuerung/fahrsteurung.ino
+++ Fahrsteuerung/fahrsteurung.ino
@@ -6,48 +6,67 @@
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "Adafruit_MCP23008.h"
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_I2CRegister.h>
+#include <Adafruit_SPIDevice.h>
+
 LiquidCrystal_I2C lcd(0x27,20,4);
+Adafruit_MCP23008 mcp;
+const uint8_t BT0 = 0; // LED an GPIO_A0
+const uint8_t BT1 = 1; // Taster an GPIO_B0
+const uint8_t ADDRESS = 0; // Adresse des MCP, Einstellen mit A0, A1, A2
+
 
 byte linksvor = 9;                           //AMT Konfiguration
 byte rechtsvor = 7;
 byte linksruck= 8;
 byte rechtsruck = 6;
                                                
-byte ML = 3;
-byte MR = 2;
-byte AL = 4;
-byte AR = 5;
+#define ML A1
+#define MR A2
+#define AL A0
+#define AR A3
 
 byte MLWert ;
 byte MRWert ;
 byte ALWert ;
 byte ARWert ;
 
-byte knopfrechts;
-byte knopflinks;
+bool knopfrechts = 1;
+bool knopflinks = 1 ;
 
-byte start;
+int blinkerFreq = 200;
+int currentBlinker = 0;
+byte blinkerSign = 1;
+byte blinkerOn; // 0 = aus, 1 = links, an 2= rechts an
+
+byte start = 255;
 char startChar;
-byte ziel;
+byte ziel = 255;
 char zielChar;
-String orte[] = {"Kreuzung-A","Kreuzung-B","Kreuzung-C","Kreuzung-D","Kreuzung-E","Kreuzung-F","Kreuzung-G","Kreuzung-H",
-                "Kreuzung-I","Kreuzung-J","Kreuzung-K","Kreuzung-L","Kreuzung-M","Kreuzung-N","Kreuzung-O","Kreuzung-P",
-                "Kreuzung-Q","Kreuzung-R","Kreuzung-S"};
-String directions[] = {"Von unten","Von links","Von Oben","Von rechts"};
+byte direction = 255;  
+String directions[] = {"Von unten","Von links","Von oben","Von rechts"};
 char orteChar[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'};
 byte ortelength = 19;
 byte weg[10];      //Streckenplan, 0 = Geradeaus ; 1 = Links ; 2 = Rechts
 byte size = 10;                              //Größe des Arrays, size() hat irgendwie nicht richtig funktioniert
 byte index = 0;
 
-byte direction = 1;       //1 nach oben, 2 nach rechts, ....  
+     //1 nach oben, 2 nach rechts, ....  
 
 byte cross = 18;
               //Die Map: Jede Kreuzung eine Spalte; nullte Zeile Name der Kreuzung; erste bis vierte Zeile Name der Nachbar Kreuzung, Reihenfolge oben, rechts, unten, links; fünfte Zeile Distanz zum Ziel
-char maps[19][7] = {"a!bc!9","b!gca9","cb!ga9","def!!9","echfd9","fei!d","g!khb9","hgkie9","ih!jf9","jno!i","k!mhg","lp!m!9","mlqnk9","n!moj9","onr!j9","p!!ql9","qpsrm9","rqs!o","sq!r!9"};
+char maps[19][7] = {"a!bc!9","b!gca9","cb!ga9","def!!9","echfd9","fei!d","g!khb9","hgkie9","ih!jf9","jno!i",
+                    "k!mhg","lp!m!9","mlqnk9","n!moj9","onr!j9","p!!ql9","qpsrm9","rqs!o","sq!r!9"};
 
 
 void setup() {
+    lcd.init();
+    lcd.backlight();
+
+
     pinMode(linksvor,OUTPUT);
     pinMode(rechtsvor,OUTPUT);
     pinMode(linksruck,OUTPUT);
@@ -58,77 +77,130 @@ void setup() {
     pinMode(AL,INPUT);
     pinMode(AR,INPUT);
 
+    mcp.begin(ADDRESS);      // use default address 0
+    mcp.pinMode(BT0, INPUT);
+    mcp.pinMode(BT1, INPUT);
+    mcp.pullUp(0, HIGH);
     Serial.begin(9600);
+
+    //  while(true){
+    //          //updateButtons();
+    //          //Serial.print(mcp.digitalRead(BT0));  
+    //          Serial.print(mcp.digitalRead(BT1));  
+    //    }
+
+    
+    
 
     getDestUI();
     startChar = orteChar[start];
     zielChar = orteChar[ziel];
     fill2(ziel);
     rout();
+
+    lcd.clear();
+    lcd.setCursor(0,1);
+    lcd.print(F("   ---       ---   "));
+    lcd.setCursor(0,2);
+    lcd.print(F("  | o |     | o | "));
+    lcd.setCursor(0,3);
+    lcd.print(F("   ---       ---   "));
 }
 
 void getDestUI(){                       //"UI" zum eingeben des Start/Ziel
-    lcd.backlight();
-    lcd.init();
+    
+    
     lcd.setCursor(0,0);
-    lcd.print("-----Wilkommen-----");
+    lcd.print(F("-----Wilkommen------"));
     lcd.setCursor(0,1);
-    lcd.print("Wandere mit Links <");
+    lcd.print(F("Wandere mit Links <"));
     lcd.setCursor(0,2);
-    lcd.print("Waehle mit Rechts >");
+    lcd.print(F("Waehle mit Rechts >"));
     lcd.setCursor(0,3);
-    lcd.print("Bestätige...");
-    while(knopfrechts== 0){
-        //knopfrechts = //PINXY;
+    lcd.print(F("Bestaetige..."));
+    while(knopfrechts == 1){
+        updateButtons();
+        //Serial.println(knopfrechts);
+        //Serial.println(knopflinks);
+    }
+    while(start == 255){
+        start = getOrt(0,true);
+    }
+    while(ziel == 255){
+        ziel = getOrt(0,false);
+    }
+    while(direction == 255){
+        direction = getDirection(0);
     }
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Waehle den Start:");
-    start = getOrt(0);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Waehle das Ziel:");
-    ziel = getOrt(0);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Aus welcher ");
+    lcd.print(F("Abbruch/Bestaetige:"));
     lcd.setCursor(0,1);
-    lcd.print("Richtung kommst du?");
-    direction = getDirection(0);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Abbruch/Bestaetige:");
-    lcd.setCursor(0,1);
-    lcd.print(orte[start]);
+    lcd.print(F("Kreuzung-"));
+    lcd.setCursor(9,1);
+    lcd.print((char) toupper(orteChar[start]));
     lcd.setCursor(0,2);
     lcd.print(directions[direction-1]+" --->");
     lcd.setCursor(0,3);
-    lcd.print(orte[ziel]);
-    while(knopflinks == 0 && knopfrechts== 0){
-        //knopfrechts = //PINXY;
-        //knopflinks = //PINXY;
+    lcd.print(F("Kreuzung-"));
+    lcd.setCursor(9,3);
+    lcd.print((char) toupper(orteChar[ziel]));
+
+    while(knopfrechts==0 || knopflinks == 0){
+        updateButtons();
     }
-    if(knopflinks){
+    while(knopflinks == 1 && knopfrechts== 1){
+        updateButtons();
+    }
+    if(knopflinks==0){
         getDestUI();
     }else{
         lcd.clear();
     }
 }
 
-byte getOrt(byte lowestOrt){                      //Rekusive Funktion zur Auswahl des Ortes
-
-    lcd.setCursor(0,2);
-    lcd.println("-> "+orte[lowestOrt]);
-
-    while(knopflinks == 0 && knopfrechts== 0){
-        //knopfrechts = //PINXY;
-        //knopflinks = //PINXY;
+void updateButtons(){
+    knopfrechts = updateButtonsDebounce(BT0,255);
+    knopflinks = updateButtonsDebounce(BT1,255);
+}
+bool updateButtonsDebounce(uint8_t BT,byte debounce){
+    //Serial.println(BT);
+    //Serial.println(mcp.digitalRead(BT)==1);
+    bool high = false;
+    for(byte i = 0; i<debounce;i++){
+        if (mcp.digitalRead(BT)==1){
+            high = true;
+            break;
+        }
     }
-    if(knopflinks){
-        if (lowestOrt == ortelength-1){
-            return getOrt(0);
+    return high;
+}
+byte getOrt(byte lowestOrt,bool start){                      //Rekusive Funktion zur Auswahl des Ortes
+    updateButtons();
+    while(knopfrechts==0 || knopflinks == 0){
+        updateButtons();
+    }
+    lcd.clear();
+    lcd.setCursor(0,0);
+    if(start){
+        lcd.print(F("Waehle den Start:"));
+    }else{
+        lcd.print(F("Waehle das Ziel:"));
+    }
+    lcd.setCursor(0,2);
+    lcd.print(F("-> Kreuzung-"));
+    lcd.setCursor(12,2);
+    lcd.print((char) toupper(orteChar[lowestOrt]));
+    
+    while(knopflinks == 1 && knopfrechts== 1){
+        updateButtons();
+        
+    }
+    if(knopflinks==0){
+        if (lowestOrt >= ortelength-1){
+            return 255;
         }else{
-        return getOrt(lowestOrt++);
+        return getOrt(lowestOrt+1,start);
         }
     }else{
         return lowestOrt;
@@ -136,19 +208,26 @@ byte getOrt(byte lowestOrt){                      //Rekusive Funktion zur Auswah
 }
 
 byte getDirection(byte currentDir){
-
-    lcd.setCursor(0,3);
-    lcd.println("-> "+directions[currentDir]);
-
-    while(knopflinks == 0 && knopfrechts== 0){
-        //knopfrechts = //PINXY;
-        //knopflinks = //PINXY;
+    updateButtons();
+    while(knopfrechts==0 || knopflinks == 0){
+        updateButtons();
     }
-    if(knopflinks){
-        if (currentDir == 3){
-            return getOrt(0);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(F("Aus welcher "));
+    lcd.setCursor(0,1);
+    lcd.print(F("Richtung kommst du?"));
+    lcd.setCursor(0,3);
+    lcd.print("-> "+directions[currentDir]);
+
+    while(knopflinks == 1 && knopfrechts== 1){
+        updateButtons();
+    }
+    if(knopflinks==0){
+        if (currentDir >= 3){
+            return 255;
         }else{
-        return getOrt(currentDir++);
+            return getDirection(currentDir+1);
         }
     }else{
         return currentDir+1;
@@ -188,7 +267,7 @@ void rout() {
     }
     index++;                            //Vorbereitung nächster Durchlauf
     for(byte i = 1; i < 5; i++){          //pointer auf nächste Kreuzung
-      if (maps[min][i] == pointer) {
+      if (maps[min][i]-97 == pointer) {
         pointer = min;
         direction = i;
       }
@@ -259,11 +338,31 @@ void kreuzungKorrektur(){                           //Richtet AMT vor Kreuzung g
     Serial.println("Korrektur Erfolgreich!");
 }
 
+void blinker(){
+    if(weg[index]!=0){
+        currentBlinker += blinkerSign;
+        if(currentBlinker==blinkerFreq ||currentBlinker == 0){
+            blinkerSign *= -1;
+            blinkerOn = !blinkerOn;
+            if(weg[index]==1){
+                digitalWrite(PINNNNNN,blinkerOn);
+            }else{
+                digitalWrite(PINNNNNN,blinkerOn);
+            }
+        }
+    }else{
+        digitalWrite(PINNNNNN,LOW);
+        digitalWrite(PINNNNNN,LOW);
+    }
+}
+
 void loop() {
 
     reset();
     scharfrechts(); 
     scharflinks();
+    	
+    blinker();
 
     while (MLWert == 0 && MRWert == 1)            //RECHTS Korrektur/Kurve fahren
     {
